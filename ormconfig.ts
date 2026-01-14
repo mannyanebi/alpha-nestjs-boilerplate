5d31963d1e4d34b1e36fbcb9d6e7e2d1eddf60fc
Emmanuel Anebi <anebiemmanuel@gmail.com>
2026-01-14 18:41:13 +0100

    refactor: enhance SSL configuration in DataSource for improved security

--- ormconfig.ts
+++ ormconfig.ts
@@ -15,6 +15,9 @@ export const dataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  ssl: process.env.DB_SSL_ENABLED === 'true' ? {
+    rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED === 'true',
+  } : false,
   namingStrategy: new SnakeNamingStrategy(),
   subscribers: [UserSubscriber],
   entities: [
